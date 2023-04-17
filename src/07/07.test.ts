e6280e9b88b7f88c9f21933359b0b03185c4ddff
avkotau <kotov.alexander1@mail.ru>
2023-04-17 16:13:48 +0200

    test, rest, destructuring

---
+++ src/07/07.test.ts
@@ -0,0 +1,19 @@
+test('', () => {
+    let houses = {
+        id: 1,
+        buildedAt: 2012,
+        repaired: false,
+        city: 'Minsk',
+        address: [{ul: 'Nezavisimosty'}, {ul: 'Pobeda'}, {ul: 'Zhukov'}]
+    }
+
+    const {id, buildedAt} = houses
+    const [, obj2, obj3] = houses.address
+
+    expect(buildedAt).toBe(2012)
+    expect(id).toBe(1)
+    expect(obj3.ul).toBe('Zhukov')
+    expect(obj2).toStrictEqual({ul: 'Pobeda'})
+})
+
+export {}
